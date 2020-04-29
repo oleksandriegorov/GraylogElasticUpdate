9642c680549b80740956084ee771c9b041a5e052
Alexander Yegorov <ayegorov@hostopia.com>
2020-04-29 07:08:10 -0400

    Kibana up to date with es6 branch

--- manifests/kibana.pp
+++ manifests/kibana.pp
@@ -6,6 +6,6 @@ class { 'kibana':
   ensure => latest,
   config => {
     'elasticsearch.hosts' => ['http://10.17.17.31:9200','http://10.17.17.32:9200'],
-    'server.host'         => '10.17.17.21',
+    'server.host'         => '10.17.17.10',
   }
 }
